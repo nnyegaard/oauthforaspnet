13a439b229d5d471afc063e03d24ba3e0d2a68d9
Jerrie Pelser <jerrie@jerriepelser.com>
2014-11-27 16:03:10 +0700

    Clean up filtering code

--- _source/js/site.js
+++ _source/js/site.js
@@ -2,18 +2,6 @@
 	$( document ).ready(function() {
 		$('#provider-list').mixItUp();
 
-
-		// $('#provider-list').find('li').filter('h2').each(function() {
-		// 	console.log($(this));
-		// });
-
-		// $('#provider-list li').each(function() {
-		// 	console.log($(this));
-		// })
-
-		//var matching = $("li h2:contains('Facebook')")
-		//console.log(matching);
-
     	$('#search').on('input', function() {
 			var searchTerm = $('#search').val().toLowerCase();
 
@@ -27,27 +15,13 @@
 					if ($(this).find('h1').text().toLowerCase().match(searchTerm))
 					{
 						$matching = $matching.add(this);
-
-						//console.log('match' + $(this));
-					} else {
-						//console.log('DOES NOT match' + $(this));
-					}
-
-					//console.log($(this).find('h1'));
+					} 
 				});
-
-				console.log($matching);
 
 				$('#provider-list').mixItUp('filter', $matching);
 			} else {
 				$('#provider-list').mixItUp('filter', 'all');
 			}
-
-			//$('#provider-list').mixItUp('filter', "li h1:contains('Facebook')");
-
-			//var matching = $("li h2:contains('Facebook')")
-
-			//console.log($('#search').val());
     	});
 	});
 })();
--- js/scripts.js
+++ js/scripts.js
@@ -9227,18 +9227,6 @@ return jQuery;
 	$( document ).ready(function() {
 		$('#provider-list').mixItUp();
 
-
-		// $('#provider-list').find('li').filter('h2').each(function() {
-		// 	console.log($(this));
-		// });
-
-		// $('#provider-list li').each(function() {
-		// 	console.log($(this));
-		// })
-
-		//var matching = $("li h2:contains('Facebook')")
-		//console.log(matching);
-
     	$('#search').on('input', function() {
 			var searchTerm = $('#search').val().toLowerCase();
 
@@ -9252,27 +9240,13 @@ return jQuery;
 					if ($(this).find('h1').text().toLowerCase().match(searchTerm))
 					{
 						$matching = $matching.add(this);
-
-						//console.log('match' + $(this));
-					} else {
-						//console.log('DOES NOT match' + $(this));
-					}
-
-					//console.log($(this).find('h1'));
+					} 
 				});
-
-				console.log($matching);
 
 				$('#provider-list').mixItUp('filter', $matching);
 			} else {
 				$('#provider-list').mixItUp('filter', 'all');
 			}
-
-			//$('#provider-list').mixItUp('filter', "li h1:contains('Facebook')");
-
-			//var matching = $("li h2:contains('Facebook')")
-
-			//console.log($('#search').val());
     	});
 	});
 })();
