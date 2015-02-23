723c627d1eb86eff81e51fe9b15021cb5a8a898d
Jerrie Pelser <jerrie@jerriepelser.com>
2015-02-23 16:28:21 +0700

    Base project. Add Facebook and Twitter examples

---
+++ src/DemoMVC5/OAuthDemoMVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OAuthDemoMVC5.Startup))]
+namespace OAuthDemoMVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
